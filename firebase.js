c22245c5a198004ed00030d2f311dc7395bc485c
Shravan-acharya608 <acharyashravan91@gmail.com>
2026-06-22 21:53:24 +0530

    Integrate Firebase Auth and update exports
    Added Firebase Authentication support and updated exports.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,43 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import { getAuth }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsr3FWkcayhv2f_UgtHmsyFytaYP7uvic",
-  authDomain: "bsmart-589e7.firebaseapp.com",
-  projectId: "bsmart-589e7",
-  storageBucket: "bsmart-589e7.firebasestorage.app",
-  messagingSenderId: "292717288196",
-  appId: "1:292717288196:web:0aeceba3e17cf150d18e51"
+
+apiKey: "YOUR_API_KEY",
+
+authDomain:
+"bsmart-589e7.firebaseapp.com",
+
+projectId:
+"bsmart-589e7",
+
+storageBucket:
+"bsmart-589e7.firebasestorage.app",
+
+messagingSenderId:
+"292717288196",
+
+appId:
+"1:292717288196:web:0aeceba3e17cf150d18e51"
+
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app =
+initializeApp(firebaseConfig);
 
-export { db };
+const db =
+getFirestore(app);
+
+const auth =
+getAuth(app);
+
+export {
+db,
+auth
+};
