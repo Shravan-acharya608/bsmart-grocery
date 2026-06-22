3c3600a63e63a80efbe31da4f789023d17b45232
Shravan-acharya608 <acharyashravan91@gmail.com>
2026-06-22 21:13:17 +0530

    Update shopkeeper.js

--- shopkeeper.js
+++ shopkeeper.js
@@ -1,61 +1,135 @@
 import { db } from "./firebase.js";
 
 import {
+
 collection,
 onSnapshot,
 doc,
 updateDoc
+
 }
+
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const ordersDiv =
 document.getElementById("orders");
 
 onSnapshot(
+
 collection(db,"orders"),
+
 (snapshot)=>{
 
 ordersDiv.innerHTML="";
 
 snapshot.forEach((order)=>{
 
-const data=order.data();
+const data =
+order.data();
 
 ordersDiv.innerHTML += `
+
 <div class="card">
 
-<h3>${data.name}</h3>
-
-<p>Phone: ${data.phone}</p>
-
-<p>Status: ${data.status}</p>
+<h3>
+${data.name}
+</h3>
 
 <p>
-Rice: ${data.order.Rice}<br>
-Milk: ${data.order.Milk}<br>
-Sugar: ${data.order.Sugar}<br>
-Oil: ${data.order.Oil}
+Order ID:
+${data.orderId}
 </p>
 
-<button onclick="readyOrder('${order.id}')">
+<p>
+Phone:
+${data.phone}
+</p>
+
+<p>
+Status:
+${data.status}
+</p>
+
+<p>
+
+Rice:
+${data.order.Rice}
+
+<br>
+
+Milk:
+${data.order.Milk}
+
+<br>
+
+Sugar:
+${data.order.Sugar}
+
+<br>
+
+Oil:
+${data.order.Oil}
+
+</p>
+
+<button
+onclick="prepareOrder(
+'${order.id}'
+)">
+Preparing
+</button>
+
+<button
+onclick="readyOrder(
+'${order.id}'
+)">
 Ready
 </button>
 
 </div>
+
 `;
 
 });
 
 }
+
 );
 
-window.readyOrder = async(id)=>{
+window.prepareOrder =
+async(id)=>{
 
 await updateDoc(
-doc(db,"orders",id),
+
+doc(
+db,
+"orders",
+id
+),
+
+{
+status:"Preparing"
+}
+
+);
+
+};
+
+window.readyOrder =
+async(id)=>{
+
+await updateDoc(
+
+doc(
+db,
+"orders",
+id
+),
+
 {
 status:"Ready for Pickup"
 }
+
 );
 
 };
